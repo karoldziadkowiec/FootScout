078fd78b8504bba4583dcd2b741b0c86bd8468c3
karoldziadkowiec <karol.dziadkkowiec@gmail.com>
2025-01-16 12:34:54 +0100

    config modified

--- FootScout.WebClient/src/config/ApiConfig.tsx
+++ FootScout.WebClient/src/config/ApiConfig.tsx
@@ -1,6 +1,4 @@
-const ApiPORT: number = 7220;    // SQL Server
-//const ApiPORT: number = 7104;    // PostgreSQL
-//const ApiPORT: number = 7272;    // MongoDB
+const ApiPORT: number = 7220;
 
 const ApiURL: string = `https://localhost:${ApiPORT}/api`;
 export default ApiURL;
--- FootScout.WebClient/src/config/ChatHubConfig.tsx
+++ FootScout.WebClient/src/config/ChatHubConfig.tsx
@@ -1,6 +1,4 @@
-const ApiPORT: number = 7220;    // SQL Server
-//const ApiPORT: number = 7104;    // PostgreSQL
-//const ApiPORT: number = 7272;    // MongoDB
+const ApiPORT: number = 7220;
 
 const HubName: string = 'chathub';
 
